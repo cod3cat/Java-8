654bf2c59e690f63ca73e2fe713f73f352bb1ec1
Praveen G <25690316+cod3cat@users.noreply.github.com>
2020-01-11 02:33:11 +0530

    Printing student data using Predicates

--- src/com/learn/java/functionalInterfaces/PredicateStudentExample.java
+++ src/com/learn/java/functionalInterfaces/PredicateStudentExample.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateStudentExample {
-    private static Predicate<Student> p1 = (student) -> student.getGradeLevel() >= 3;
+    static Predicate<Student> p1 = (student) -> student.getGradeLevel() >= 3;
     private static Predicate<Student> p2 = (student) -> student.getGpa() >= 3.9;
 
     public static void main(String[] args) {
